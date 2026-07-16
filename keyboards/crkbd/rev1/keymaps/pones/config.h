8adad820dc42e7e4443193be623343102f9d91e5
Pablo Ontiveros <pablo.ontiveros@gmail.com>
2026-07-16 07:39:17 -0600

    fix(keymap): disable digital rain and framebuffer effects to fit firmware size

--- keyboards/crkbd/rev1/keymaps/pones/config.h
+++ keyboards/crkbd/rev1/keymaps/pones/config.h
@@ -36,7 +36,7 @@
 #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 #   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS // disabled: firmware size limit
 #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #   define RGB_MATRIX_HUE_STEP 8
@@ -69,7 +69,7 @@
 #    undef ENABLE_RGB_MATRIX_RAINDROPS
 #    undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
 #    undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
-#   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+//  #   define ENABLE_RGB_MATRIX_DIGITAL_RAIN // disabled: firmware size limit
 #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
 #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
